4a63eb9b37c64002db7d2717c1cdcc7ab182e368
amani <ellafi.amani@gmail.com>
2022-04-15 15:52:32 +0200

    updtae DAG

--- dags/test_dag_amani.py
+++ dags/test_dag_amani.py
@@ -4,6 +4,7 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.contrib.sensors.file_sensor import FileSensor
 from airflow.operators.dummy_operator   import DummyOperator
+from airflow.operators.python import BranchPythonOperator
 
 
 default_args = {
@@ -11,7 +12,6 @@ default_args = {
     'depends_on_past': False,
     'email': ['airflow@example.com'],
     'start_date': datetime(2022, 3, 27),
-
     'email_on_failure': False,
     'email_on_retry': True,
     'retries': 1,
@@ -22,6 +22,29 @@ dag = DAG('test_dag_amani', default_args=default_args, schedule_interval="@daily
 
 existing_file = FileSensor(
     task_id='scheduler.log',
-    filepath="/home/amani/airflow/logs/scheduler/2022-04-15/test_dag_amani.py.log",
+    filepath="/home/julien/airflow/logs/scheduler/{{ ds }}/scheduler.py.log",
     dag=dag
 )
+
+def check_errors(filepath):
+    pattern = "ERROR"
+    error_count = 0
+    with open(filepath,"r") as file:
+        for line in file:
+            if re.search(pattern, line):
+                error_count += 1
+    if error_count > 50:
+        return "send_mail"
+    return "do_nothing"
+
+count_errors = BranchPythonOperator(
+    task_id="count_errors",
+    python_callable=check_errors,
+    op_kwargs={'filepath': '/home/julien/airflow/logs/scheduler/{{ ds }}/scheduler.py.log'},
+    dag=dag
+)
+
+send_mail = DummyOperator(task_id="send_mail")
+do_nothing = DummyOperator(task_id="do_nothing")
+
+existing_file >> count_errors >> [send_mail, do_nothing]
